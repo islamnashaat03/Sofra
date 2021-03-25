57f80d2048dedd6ff207f880b7ff84129f6b7683
islamnashaat03 <islamnashaat03@gmail.com>
2021-03-25 02:37:51 +0200

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
-
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    rtl:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+            loop:true
+        },
+        600:{
+            items:3,
+            nav:false,
+            loop:true
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    }
+})
